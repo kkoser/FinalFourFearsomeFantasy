7d957d87c20cf3a89cf55e502b34d960fbc25b06
kkoser <kkoser@nd.edu>
2014-04-27 02:01:18 -0400

    Added MainCharacter saving on deletion

--- MainCharacter.cpp
+++ MainCharacter.cpp
@@ -19,5 +19,25 @@ void MainCharacter::writeToFile(string fileName) {
     ofstream file;
     file.open(fileName.c_str());
     
+    file << name << endl;
+    file << maxHealth << endl;
+    file << standardPower << endl;
+    file << maxPP <<endl;
+    file << standardPPRegen << endl;
+    file << currentHealth << endl;
+    file << exp << endl;
+    file << spriteName << endl;
+    
+    string fullmoves = "";
+    typename vector<string>::iterator move;
+    for (move = moves.begin(); move != moves.end(); ++move) {
+        if (fullmoves.compare("") != 0) {
+            fullmoves = fullmoves + ","; //append a comma at the end of the previous move, but only if there is a previous move
+        }
+        fullmoves = fullmoves + *move;
+    }
+    
+    file.close();
+    
     
 }
